97e98067e579152c015b7bd75e1eed79b5aa0a9b
chrism-48 <moore4020@gmail.com>
2022-05-10 03:41:10 -0400

    added more animations

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 
 
-// anime({
-//     targets: 'div.boxes.two',
-//     translateY: 250,
-//     rotate: '10turn',
-//     backgroundColor: '#FFF',
-//     duration: 800
-//   });
+
+
+anime({
+    targets: 'div.pageTitle',   
+    scale:0,      
+});
+
+anime({
+    targets: 'div.pageTitle',   
+    scale:3,   
+    easing: 'easeInOutExpo',
+    duration:4000,
+    rotate: {
+        value: 360,
+        duration: 1800,
+        easing: 'easeInOutSine'
+      }   
+});
+
+
+anime({
+    targets: 'div.pageTitle2',   
+    scale:0,      
+});
+
+anime({
+    targets: 'div.pageTitle2',   
+    scale:3,   
+    easing: 'easeInOutExpo',
+    duration:4000,
+    rotate: {
+        value: 360,
+        duration: 1800,
+        easing: 'easeInOutSine'
+      }   
+});
 
 
 const myStart = document.querySelector('#start');
 myStart.addEventListener('click', spin);
 const myReset = document.querySelector('#reset');
 myReset.addEventListener('click', reset);
-
-
+const myReset2 = document.querySelector('#reset2');
+myReset2.addEventListener('click', reset2);
+const myStart2 = document.querySelector('#start2');
+myStart2.addEventListener('click', animation2);
 
 
 function spin() {
@@ -81,11 +112,74 @@ function spin() {
         
        
     });
+    
+
 }
+
+
+
+
+function animation2() {
+    anime({
+        targets: 'div.boxes.five',   
+        scale:1.5,   
+        easing: 'easeInOutExpo',
+        duration:4000,
+        rotate: {
+            value: 360,
+            duration: 1800,
+            easing: 'easeInOutSine'
+        }          
+    });
+    anime({
+        targets: 'div.boxes.six',   
+        scale:1.5,   
+        easing: 'easeInOutExpo',
+        duration:4000,
+        rotate: {
+            value: 360,
+            duration: 1800,
+            easing: 'easeInOutSine'
+        }          
+    });
+    anime({
+        targets: 'div.boxes.seven',   
+        scale:1.5,   
+        easing: 'easeInOutExpo',
+        duration:4000,
+        rotate: {
+            value: 360,
+            duration: 1800,
+            easing: 'easeInOutSine'
+        }          
+    });
+    anime({
+        targets: 'div.boxes.eight',   
+        scale:1.5,   
+        easing: 'easeInOutExpo',
+        duration:4000,
+        rotate: {
+            value: 360,
+            duration: 1800,
+            easing: 'easeInOutSine'
+        }          
+    });
+}
+
+
 
 
 function reset() {
     location.reload();
 }
+
+
+
+
+
+function reset2() {
+    location.reload();
+}
+
 
 
